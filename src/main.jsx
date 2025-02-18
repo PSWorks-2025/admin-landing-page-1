21337bb0514de5bf0ad8441eccf49af82e53c286
NewToCodeNow <kenleweb13@gmail.com>
2025-02-18 22:44:29 +0700

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import App from "./App";
 import "./index.css";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter basename="/admin-landing-page-1">
+  <HashRouter >
     <Routes>
       <Route path="/" element={<App />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
